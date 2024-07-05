fc04db880d8aae044d8e79761eafbfe39013f704
pawan-exe <pawangupta63431@gmail.com>
2024-07-05 10:23:09 +0530

    Added Custom tabs project

---
+++ 9. Custom Tabs/main.js
@@ -0,0 +1,24 @@
+const tabContainer = document.querySelector(".container");
+const tabButtons = document.querySelectorAll(".tab-button");
+const tabContents = document.querySelectorAll(".content");
+
+tabContainer.addEventListener("click", (event) => {
+  console.log(event.target.dataset);
+
+  const currentId = event.target.dataset.id;
+
+  if (currentId) {
+    tabButtons.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+
+    event.target.classList.add("active");
+
+    tabContents.forEach((content) => {
+      content.classList.remove("active");
+    });
+
+    const currentContent = document.getElementById(currentId);
+    currentContent.classList.add("active");
+  }
+});
